cacb8b96cd514b52b766c8f0f1b985e942cda1a2
Kryptipos <kryptipos@gmail.com>
2026-03-02 19:45:40 +0700

    Clear legacy guild commands before global deploy

--- deploy-commands.js
+++ deploy-commands.js
@@ -130,6 +130,13 @@ const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_TOKEN);
 
 (async () => {
     try {
+        if (process.env.GUILD_ID) {
+            console.log("... Clearing legacy guild commands...");
+            await rest.put(
+                Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+                { body: [] }
+            );
+        }
         console.log("... Registering global commands...");
         await rest.put(
             Routes.applicationCommands(process.env.CLIENT_ID),
